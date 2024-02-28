cfcfb160842479ce1fc2eb731357f3276ddfb709
Oskar Swat <oskar.swat@tlen.pl>
2024-02-28 17:47:58 +0100

    defined controlled - 22:30

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,13 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _prams) do
+    users = [
+      %{ id: 1, name: "Oskar", email: "mojemail@gmail.com"},
+      %{ id: 2, name: "Maja", email: "majamail@email.pl"},
+    ]
+
+    json(conn, %{users: users})
+  end
 end
--- lib/forum_web/router.ex
+++ lib/forum_web/router.ex
@@ -18,6 +18,7 @@ defmodule ForumWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/users", PageController, :users
   end
 
   # Other scopes may use custom stacks.
