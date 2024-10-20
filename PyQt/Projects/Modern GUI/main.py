9d2474a223e63b0065dbf95790e3e8923e0b1846
Oskar Swat <oskar.swat@tlen.pl>
2024-10-20 16:33:24 +0200

    welcome screen

--- PyQt/Projects/Modern GUI/main.py
+++ PyQt/Projects/Modern GUI/main.py
@@ -1,6 +1,24 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
-from PyQt5.QtWidgets import QApplication, QDialog, QWidget
+from PyQt5.QtWidgets import QApplication, QDialog, QWidget, QStackedWidget
 
 
+class WelcomeScreen(QDialog):
+    def __init__(self):
+        super(WelcomeScreen, self).__init__()
+        loadUi('welcomescreen.ui', self)
+
+# main
+app = QApplication(sys.argv)
+mainwindow = WelcomeScreen()
+widget = QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedHeight(800)
+widget.setFixedWidth(1200)
+widget.show()
+
+try:
+    sys.exit(app.exec_())
+except:
+    print("Exiting...")
