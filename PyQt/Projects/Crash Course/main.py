76df6068624098204d88131f524a70a736b33d3d
Oskar Swat <oskar.swat@tlen.pl>
2024-10-20 14:14:57 +0200

    login app, crash course

--- PyQt/Projects/Crash Course/main.py
+++ PyQt/Projects/Crash Course/main.py
@@ -9,8 +9,23 @@ class MainWindow(QDialog):
         super(MainWindow, self).__init__()
         loadUi("qtcrashcourse.ui", self)
 
-    # Button
+        # Button
         self.button.clicked.connect(self.button_clicked)
+
+        # Check Box
+        self.checkBox.stateChanged.connect(self.checked)
+
+        # Combo Box
+        self.comboBox.setVisible(False)
+        list_zawod = ["Lekarz", "Policjant", "Strażak", "Informatyk"]
+        for job in list_zawod:
+            self.comboBox.addItem(job)
+        self.comboBox.currentIndexChanged.connect(self.combochanged)
+
+        # Spin Box
+        self.spinBox.setMinimum(18)
+        self.spinBox.setMaximum(99)
+        self.spinBox.valueChanged.connect(self.spinchanged)
 
     def button_clicked(self):
         output_str = self.name.toPlainText() + " " + self.surname.toPlainText()
@@ -18,7 +33,24 @@ class MainWindow(QDialog):
         self.surname.setReadOnly(True)
         self.name.setDisabled(True)
         self.surname.setDisabled(True)
+        output_str += ", lat " + str(self.spinBox.value())
+        if self.checkBox.isChecked():
+            output_str += ", jest zatrudniony"
+        else:
+            output_str += ", nie zatrudniony"
         print(output_str)
+
+    def checked(self):
+        if self.checkBox.isChecked():
+            self.comboBox.setVisible(True)
+        else:
+            self.comboBox.setVisible(False)
+
+    def combochanged(self):
+        self.outputlabel.setText(self.comboBox.currentText() + " jest wybrany")
+
+    def spinchanged(self):
+        print(self.spinBox.value())
 
 # main
 app = QApplication(sys.argv)
