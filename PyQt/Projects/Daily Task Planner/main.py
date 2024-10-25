1d7c2b927451be30232da790050a3b3bc424ef22
Oskar Swat <oskar.swat@tlen.pl>
2024-10-25 13:49:43 +0200

    List checkbox

--- PyQt/Projects/Daily Task Planner/main.py
+++ PyQt/Projects/Daily Task Planner/main.py
@@ -1,17 +1,30 @@
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QListWidgetItem
 from PyQt5.uic import loadUi
 import sys
+from PyQt5 import QtCore
+
+
+tasks = ["Napisać emaila", "Obejrzeć film", "Zadzwonić do mamy"]
 
 class Window(QWidget):
     def __init__(self):
         super(Window, self).__init__()
         loadUi('main.ui', self)
         self.calendarWidget.selectionChanged.connect(self.calendarDateChanged)
+        self.updateTaskList()
 
     def calendarDateChanged(self):
         print("Calendar date changed")
         dateSelected = self.calendarWidget.selectedDate().toPyDate().strftime('%d.%m.%Y')
         print(dateSelected)
+
+    def updateTaskList(self):
+        for task in tasks:
+            # add to the list widget
+            item = QListWidgetItem(task)
+            item.setFlags(item.flags() | QtCore.Qt.ItemIsUserCheckable)
+            item.setCheckState(QtCore.Qt.Unchecked)
+            self.taskslistWidget.addItem(item)
 
 
 if __name__ == '__main__':
