5f768f47f9ea3b28b527879a3d94e0adc09ae4a9
Oskar Swat <oskar.swat@tlen.pl>
2024-11-04 14:40:15 +0100

    Send function init

--- PyQt/Projects/Email sender/main.py
+++ PyQt/Projects/Email sender/main.py
@@ -1,11 +1,16 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
 import sys
+from email_sender import send_email
 
 class EmailSender(QMainWindow):
     def __init__(self):
         super(EmailSender, self).__init__()
         loadUi('main.ui', self)
+        self.pushButton.clicked.connect(self.SendButtonPushed)
+
+    def SendButtonPushed(self):
+        send_email(recipient=self.lineEdit.text(), email=self.textEdit.toPlainText())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
