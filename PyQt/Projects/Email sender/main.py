6f9d648079898cd1de705122ff2c9e6f435ac89f
Oskar Swat <oskar.swat@tlen.pl>
2024-11-04 14:26:35 +0100

    email sender init

---
+++ PyQt/Projects/Email sender/main.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+import sys
+
+class EmailSender(QMainWindow):
+    def __init__(self):
+        super(EmailSender, self).__init__()
+        loadUi('main.ui', self)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = EmailSender()
+    window.show()
+    app.exec_()
