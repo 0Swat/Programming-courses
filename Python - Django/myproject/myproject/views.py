ed8d4aeca5a15a89fe7c9bbbe199545164555362
Oskar Swat <oskar.swat@tlen.pl>
2025-01-21 01:09:48 +0100

    Lesson 1 - static, templates, basic page

---
+++ Python - Django/myproject/myproject/views.py
@@ -0,0 +1,10 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello World! I'm home.")
+    return render(request, 'home.html')
+
+def about(request):
+    # return HttpResponse("My About Page")
+    return render(request, 'about.html')
