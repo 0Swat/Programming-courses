f5d9fe9b2b46831cc8e6bb3d392c40d40ac23961
Oskar Swat <oskar.swat@tlen.pl>
2025-01-23 00:52:59 +0100

    new app - user register (video task)

--- Python - Django/myproject/myproject/urls.py
+++ Python - Django/myproject/myproject/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about', views.about),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('users/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
