9584baa362b74469af14374c59e10293241114ab
Oskar Swat <oskar.swat@tlen.pl>
2025-01-24 14:20:47 +0100

    make post form

--- Python - Django/myproject/posts/views.py
+++ Python - Django/myproject/posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from . import forms
+
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all().order_by('date')
@@ -12,4 +14,13 @@ def post_page(request, slug):
 
 @login_required(login_url="/users/login/")
 def post_new(request):
-    return render(request, 'posts/post_new.html')
+    if request.method == "POST":
+        form = forms.CreatePost(request.POST, request.FILES)
+        if form.is_valid():
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+        return render(request, 'posts/post_new.html', { 'form': form })
